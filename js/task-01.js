c3d8e3c040090eec8671f107404026ea5bb25144
vladyslavketov <106628608+vladyslavketov@users.noreply.github.com>
2022-09-30 01:18:21 +0300

    task-02 - done

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,14 @@
-const categoriesListRef = document.querySelectorAll('#categories li.item');
-console.log(`Number of categories: ${categoriesListRef.length}`);
+const categoriesItemsRef = document.querySelectorAll('#categories li.item');
+console.log(`Number of categories: ${categoriesItemsRef.length}`);
 
-for (let i = 0; i < categoriesListRef.length; i += 1) {
-  console.log(`Category: ${categoriesListRef[i].querySelector('h2').textContent}`);
-  console.log(`Elements: ${categoriesListRef[i].querySelectorAll('li').length}`);
-}
+// варіант 1
+for (let i = 0; i < categoriesItemsRef.length; i += 1) {
+  console.log(`Category: ${categoriesItemsRef[i].querySelector('h2').textContent}`);
+  console.log(`Elements: ${categoriesItemsRef[i].querySelectorAll('li').length}`);
+};
+
+// варіант 2
+// categoriesItemsRef.forEach(function (item) {
+//   console.log(`Category: ${item.querySelector('h2').textContent}`);
+//   console.log(`Elements: ${item.querySelectorAll('li').length}`);
+// });
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListRef = document.querySelector('ul#ingredients');
+const ingredientsItemsRef = ingredients.map(ingredientName => {
+  const liRef = document.createElement('li');
+
+  liRef.textContent = ingredientName;
+  liRef.classList.add('item');
+
+  return liRef;
+});
+
+ingredientsListRef.append(...ingredientsItemRef);
